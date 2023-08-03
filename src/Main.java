e0c0fd475b8044d5de53f9f2b265db2b5aa8b033
Denis Trinev <den74vvo@gmail.com>
2023-08-03 21:37:13 +1000

    Error Listener added

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@ public class Main {
     public static void main(String[] args) {
 
         OnTaskDoneListener listener = System.out::println;
+        OnTaskErrorListener errorListener = System.out::println;
 
-        Worker worker = new Worker(listener);
+        Worker worker = new Worker(listener, errorListener);
         worker.start();
     }
 }
--- src/Worker.java
+++ src/Worker.java
@@ -1,10 +1,18 @@
 public class Worker {
     private OnTaskDoneListener callback;
-    public Worker(OnTaskDoneListener callback){
-        this.callback=callback;
+    private OnTaskErrorListener errorCallback;
+
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
+        this.callback = callback;
+        this.errorCallback = errorCallback;
     }
+
     public void start() {
         for (int i = 0; i < 100; i++) {
+            if (i == 33) {
+                errorCallback.onError("Task " + i + " occurred error!");
+                continue;
+            }
             callback.onDone("Task " + i + " is done");
         }
     }
